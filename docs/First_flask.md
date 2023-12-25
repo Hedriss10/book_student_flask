f12fe01bf1ce46542a5b33569b2d89c6ec40f0df
Hedriss10 <hedrisgts@gmail.com>
2023-12-25 13:14:06 -0300

    " flask student "

--- docs/First_flask.md
+++ docs/First_flask.md
@@ -3,4 +3,4 @@
 
 ### Flask é micro-framewrok
 
-Aprendir que podemos criar funções e renderizar no campo do HTML de forma simples e descomplicada, diferente do djago que não entrava na minha cabeça e não fazia sentido. Também aprendir fazer uma conexão com `class` instanciando simples
+Aprendir que podemos criar funções e renderizar no campo do HTML de forma simples e descomplicada, diferente do djago que não entrava na minha cabeça e não fazia sentido. Também aprendir fazer uma conexão com `class` instanciando objetos simples, também aprendir requsição de formularios aonde podemos buscar o `GET` e `POST` e redenrizando em rotas especificas.
--- jogo_teca/jogoteca.py
+++ jogo_teca/jogoteca.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template
-
-
-
+from flask import Flask, render_template, request
 
 class Jogos:
     def __init__(self, name, console, categoria):
@@ -10,21 +7,35 @@ class Jogos:
         self.categoria = categoria
 
 
+# passando a quantidade de jogos que teremos
+jogo1 = Jogos(name="Dota", console="PC", categoria="RPG")
+jogo2 = Jogos(name="Warcraft", console="PC", categoria="RPG")
+jogo3 = Jogos(name="LOL", console="PC", categoria="RPG")
+list_game = [jogo1, jogo2, jogo3]
 
 
 app = Flask(__name__)
 
 @app.route("/inicio")
 def home_ola():
-    
-    # passando a quantidade de jogos que teremos
-    jogo1 = Jogos(name="Dota", console="PC", categoria="RPG")
-    jogo2 = Jogos(name="Warcraft", console="PC", categoria="RPG")
-    jogo3 = Jogos(name="LOL", console="PC", categoria="RPG")
-    
-    list_game = [jogo1, jogo2, jogo3]
-    
+        
     return render_template("lista.html", titulos='jogos', jogos=list_game)
     
+    
+    
+@app.route("/novo")
+def novo():
+    return render_template("novo.html", titulo="Novos jogos")
+    
+
+@app.route("/criar", methods=["POST", ])
+def criar():
+    nome = request.form["nome"]
+    categoria = request.form["categoria"]
+    console = request.form["console"]
+    novo_jogo = Jogos(name=nome, categoria=categoria, console=console)
+    list_game.append(novo_jogo)
+    return render_template("novo.html", titulos="jogos", jogos=list_game)
+
 if __name__ == "__main__":
     app.run(port=8000 , debug=True)
