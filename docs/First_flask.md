f261ac848fb9d2e14c79b9a8f562fd2da6dd66eb
Hedriss10 <hedrisgts@gmail.com>
2023-12-25 12:50:10 -0300

    " modulo finalizado "

---
+++ docs/First_flask.md
@@ -0,0 +1,6 @@
+### Primeiros passos em Flask 
+
+
+### Flask é micro-framewrok
+
+Aprendir que podemos criar funções e renderizar no campo do HTML de forma simples e descomplicada, diferente do djago que não entrava na minha cabeça e não fazia sentido. Também aprendir fazer uma conexão com `class` instanciando simples
--- jogo_teca/jogoteca.py
+++ jogo_teca/jogoteca.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
 
+
+
+
+class Jogos:
+    def __init__(self, name, console, categoria):
+        self.name =  name 
+        self.console = console
+        self.categoria = categoria
+
+
+
+
 app = Flask(__name__)
 
 @app.route("/inicio")
 def home_ola():
-    list_game = ['Dota', 'Pes2012', 'Warcraft', 'Need for speed', 'SubwaySurfs']
+    
+    # passando a quantidade de jogos que teremos
+    jogo1 = Jogos(name="Dota", console="PC", categoria="RPG")
+    jogo2 = Jogos(name="Warcraft", console="PC", categoria="RPG")
+    jogo3 = Jogos(name="LOL", console="PC", categoria="RPG")
+    
+    list_game = [jogo1, jogo2, jogo3]
+    
     return render_template("lista.html", titulos='jogos', jogos=list_game)
     
 if __name__ == "__main__":
